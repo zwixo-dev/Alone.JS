ea9c1ec422a6c616ab6775df5bd53315799794ba
SaadDev <zwixozouine@gmail.com>
2026-06-14 19:33:06 +0100

    add matrix.h header in include folder

---
+++ include/matrix.h
@@ -0,0 +1,17 @@
+#ifndef MATH_H
+#define MATH_H
+
+
+double array_sum(int size, double *arr);
+
+double array_product(int size, double *arr);
+
+double array_average(int size, double *arr);
+
+double array_min(int size, double *arr);
+
+double array_max(int size, double *arr);
+
+double array_range(int size, double *arr);
+
+#endif
