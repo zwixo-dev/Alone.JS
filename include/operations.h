bf7bde27d3bf9b344aede0a951c99e25b0527ff5
SaadDev <zwixozouine@gmail.com>
2026-06-14 19:31:53 +0100

    add operations.h header in include folder

---
+++ include/operations.h
@@ -0,0 +1,16 @@
+#ifndef MATH_H
+#define MATH_H
+
+double addition(double a, double b);
+double subtract(double a, double b);
+double multiply(double a, double b);
+double divide(double a, double b);
+
+double power(double  base, double  exponent);
+double square(double n);
+double cube(double n);
+double sqrt(double n);
+double aBs(int n);
+
+
+#endif
