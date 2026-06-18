760f1f4dae9c65f45d2bb9e608118e7d248f6dec
SaadDev <zwixozouine@gmail.com>
2026-06-18 16:51:39 +0100

    add sorting func modifie the array_mode && array_mean && array_median

--- src/statistics.c
+++ src/statistics.c
@@ -1,43 +1,50 @@
 #include "../include/statistics.h"
 #include "../include/operations.h"
 
+// adding the sort function instead of write each time inside the functions 
+
+void sorting(int size, double *arr){
+    double temp;
+
+    // sorting
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = i + 1; j < size; j++) { 
+            if (arr[i] > arr[j]) {   
+                temp = arr[i]; 
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+}
+
 // array_mean
-double array_mean(int size, double *getArr){
+double array_mean(int size, double *arr){
+
+    if(size<=0) return 0.0;
+
     double count = 0.0;
     
     for(int i=0; i<size; i++){
-        count += getArr[i];
+        count += arr[i];
     }
     
     return count / size;
 }
 
 //array_median
-double array_median(int size, double *getArr) {
-    double temp;
-    double tempArr[size];
+double array_median(int size, double *arr) {
     
-    for(int i = 0; i < size; i++) {
-    tempArr[i] = getArr[i];
-    }
+    if(size<=0) return -1; // failed
 
-    // sorting
-    for (int i = 0; i < size - 1; i++) {
-        for (int j = i + 1; j < size; j++) { 
-            if (tempArr[i] > tempArr[j]) {   
-                temp = tempArr[i]; 
-                tempArr[i] = tempArr[j];
-                tempArr[j] = temp;
-            }
-        }
-    }
-    
+    sorting(size, arr);    
+
     int middle = size / 2; 
     
     if (size % 2 != 0) {
-        return tempArr[middle];
+        return arr[middle];
     } else {
-        return (tempArr[middle - 1] + tempArr[middle]) / 2.0; 
+        return (arr[middle - 1] + arr[middle]) / 2.0; 
     }
 }
 
@@ -72,6 +79,10 @@ double array_stddev(int size, double *arr){
 // array_mode
 
 double array_mode(int size, double *arr) {
+
+    if(size<=0) return -1.0;
+    if(size==1) return arr[0];
+
     int count = 0;
     int maxCount = 0; 
     double maxValue = 0.0;
