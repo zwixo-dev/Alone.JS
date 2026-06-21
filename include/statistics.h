a60e61826a2771e422f88d7c3dcab78f8259bfba
SaadDev <zwixozouine@gmail.com>
2026-06-21 01:24:59 +0100

    add option parameter to stddev && skewness && cov and correlation for sample and population calculations

--- include/statistics.h
+++ include/statistics.h
@@ -8,7 +8,7 @@ double array_median(int size, double *arr);
 
 double array_variance(int size, double *arr, int option);
 
-double array_stddev(int size, double *arr);
+double array_stddev(int size, double *arr, int option);
 
 double array_mode(int size, double *arr);
 
@@ -18,14 +18,14 @@ double array_quartiles(int size, double *arr, double Q);
 
 double array_iqr(int size, double *arr);
 
-double array_skewness(int size, double *arr);
+double array_skewness(int size, double *arr, int option);
 
 double array_kurtosis(int size, double *arr, double s);
 
-double array_covariance(int size, double *arr_x, double *arr_y);
+double array_covariance(int size, double *arr_x, double *arr_y, int option);
 
 void array_normalize(int size, double *arr, double *output_arr);
 
-double array_correlation(int size, double *arr_x, double *arr_y);
+double array_correlation(int size, double *arr_x, double *arr_y, int option);
 
 #endif
