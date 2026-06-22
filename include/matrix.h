b1e9ab2a7345f3c33eb4fdd86d73895318cf7936
SaadDev <zwixozouine@gmail.com>
2026-06-22 20:57:33 +0100

    add comments to complete the code

--- include/matrix.h
+++ include/matrix.h
@@ -1,6 +1,7 @@
 #ifndef ALONE_ARRAY_UTILS_H
 #define ALONE_ARRAY_UTILS_H
 
+// simple array
 
 double array_sum(int size, double *arr);
 
@@ -13,5 +14,7 @@ double array_min(int size, double *arr);
 double array_max(int size, double *arr);
 
 double array_range(int size, double *arr);
+
+// 2d array
 
 #endif
