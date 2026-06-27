a6dd29ab1d5ca9de8a576a34f5205f89cbee2b6c
SaadDev <zwixozouine@gmail.com>
2026-06-28 00:21:28 +0100

    delete the test

--- wrappers/operationsWrapper.js
+++ wrappers/operationsWrapper.js
@@ -13,18 +13,5 @@ Module.onRuntimeInitialized = () => {
         sqrt: Module.cwrap("sqrt", "number", ["number","number"]),
         aBs: Module.cwrap("aBs", "number", ["number","number"]),
     }
-
-
-    // fast testing 
-    console.log(operations.addition(1, 2));
-    console.log(operations.subtract(1, 2));
-    console.log(operations.multiply(1, 2));
-    console.log(operations.divide(1, 2));
-    console.log(operations.power(1, 2));
-    console.log(operations.square(2));
-    console.log(operations.cube(2));
-    console.log(operations.sqrt(2));
-    console.log(operations.aBs(-2));
-
-
+    
 };
