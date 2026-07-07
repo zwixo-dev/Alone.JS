1ce2dbc53b6b61d14fb360177c1ea62caf5eaaa9
SaadDev <zwixozouine@gmail.com>
2026-07-07 22:05:18 +0100

    give the params

---
+++ include/probability.h
@@ -0,0 +1,20 @@
+#ifndef ALONE_GEOMETRY_UTILS_H
+#define ALONE_GEOMETRY_UTILS_H
+
+double factorial(int n);
+
+double permutation(int n, int r);
+
+double combination(int n, int r);
+
+double binomial_probability(int n, int x, double p);
+
+double poisson_probability(int k, double lambda);
+
+double normal_pdf(double x, double mean, double stddev);
+
+double normal_cdf(double x, double mean, double stddev);
+
+double bernoulli(double x, double p);
+
+#endif
