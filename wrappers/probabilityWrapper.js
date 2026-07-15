5db5e383c14104446c28a333bb9057154f52de8e
SaadDev <zwixozouine@gmail.com>
2026-07-15 23:58:11 +0100

    test logistic_cdf and pdf (forgot to compile pdf func here)

--- wrappers/probabilityWrapper.js
+++ wrappers/probabilityWrapper.js
@@ -25,8 +25,8 @@ Module.onRuntimeInitialized = () =>{
     standard_normal_cdf:  Module.cwrap("standard_normal_cdf", "number", ["number"]),
     weibull_pdf:  Module.cwrap("weibull_pdf", "number", ["number", "number", "number"]),
     weibull_cdf:  Module.cwrap("weibull_cdf", "number", ["number", "number", "number"]),
-    // logistic_pdf:  Module.cwrap("logistic_pdf", "number", ["number", "number"]),
-    // logistic_cdf:  Module.cwrap("logistic_cdf", "number", ["number", "number"]),
+    logistic_pdf:  Module.cwrap("logistic_pdf", "number", ["number", "number", "number"]),
+    logistic_cdf:  Module.cwrap("logistic_cdf", "number", ["number", "number", "number"]),
     // cauchy_pdf:  Module.cwrap("cauchy_pdf", "number", ["number", "number"]),
     // cauchy_cdf:  Module.cwrap("cauchy_cdf", "number", ["number", "number"]),
     // chi_square_pdf:  Module.cwrap("chi_square_pdf", "number", ["number", "number"]),
@@ -53,9 +53,10 @@ Module.onRuntimeInitialized = () =>{
     console.log("uniform_cdf : ", probability.uniform_cdf(6, 2, 10  ));
     console.log("standard_normal_pdf : ", probability.standard_normal_pdf(0));
     console.log("standard_normal_cdf : ", probability.standard_normal_cdf(0));
-    console.log("weibull_pdf", probability.weibull_pdf(8, 2, 10));
-    console.log("weibull_cdf", probability.weibull_cdf(8, 2, 10));
-
+    console.log("weibull_pdf : ", probability.weibull_pdf(8, 2, 10));
+    console.log("weibull_cdf : ", probability.weibull_cdf(8, 2, 10));
+    console.log("logistic_pdf : ", probability.logistic_pdf(0, 0, 1));
+    console.log("logistic_cdf", probability.logistic_cdf(0, 0, 1));
 }
 
 
