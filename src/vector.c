9934d4505b9e4c2b359cafcc2432dcca7beff481
SaadDev <zwixozouine@gmail.com>
2026-07-18 22:48:51 +0100

    implement vector_normalize func

--- src/vector.c
+++ src/vector.c
@@ -14,3 +14,14 @@ double vector_magnitude(int size, double *vector){
     
     return sqrt(vectors_result);
 }
+
+// vector_normalize
+void vector_normalize(int size, double *vector, double *result_vectors){
+    if (size<=0 || vector == NULL || result_vectors == NULL) return;
+
+    double Vmagnitude = vector_magnitude(size, vector);
+    
+    for(int i=0; i<size; i++){
+        result_vectors[i] = ( vector[i] / Vmagnitude);
+    }
+}
