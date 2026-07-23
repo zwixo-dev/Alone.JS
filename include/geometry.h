e17d8c89af1db60bd0c5c410666ed7bcff2a9516
SaadDev <zwixozouine@gmail.com>
2026-07-23 23:47:57 +0100

    setup Triangle headers

--- include/geometry.h
+++ include/geometry.h
@@ -10,5 +10,16 @@ double geometry_midpoint_x(double x1, double x2);
 double geometry_midpoint_y(double y1, double y2);
 double geometry_slope(double x1, double y1, double x2, double y2);
 
+// Triangle
+
+double triangle_area(double base, double height);
+double triangle_perimeter(double a, double b, double c);
+double triangle_heron_area(double a, double b, double c);
+double triangle_semiperimeter(double a, double b, double c);
+int triangle_is_valid(double a, double b, double c);
+int triangle_is_equilateral(double a, double b, double c);
+int triangle_is_isosceles(double a, double b, double c);
+int triangle_is_scalene(double a, double b, double c);
+int triangle_is_right(double a, double b, double c);
 
 #endif
