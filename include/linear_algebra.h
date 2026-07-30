9e379ca8ef561da9c9608354369a7533ace1ba14
SaadDev <zwixozouine@gmail.com>
2026-07-30 23:26:16 +0100

    add more Interpolation func headers

--- include/linear_algebra.h
+++ include/linear_algebra.h
@@ -10,5 +10,14 @@ double remap(double value, double in_min, double in_max, double out_min, double 
 
 double smoothstep(double edge0, double edge1, double x);
 
+double smootherstep(double edge0, double edge1, double x);
+
+double clamp(double value, double min, double max);
+
+void lerp_vector(int size, const double *a, const double *b, double t, double *result);
+
+void lerp_points_2d(const double *a, const double *b, double t, double *result);
+
+void lerp_points_3d(const double *a, const double *b, double t, double *result);
 
 #endif
