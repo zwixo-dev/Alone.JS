b041d6c5d2288832e802045c24de6ec47c1310cf
SaadDev <zwixozouine@gmail.com>
2026-07-30 23:29:42 +0100

    add more 2D coordinate transformations headers

--- include/linear_algebra.h
+++ include/linear_algebra.h
@@ -32,4 +32,10 @@ void shear_point_2d(double x, double y, double shear_x, double shear_y, double *
 
 void reflect_point_2d(double x, double y, double axis_x, double axis_y, double *result_x, double *result_y);
 
+void transform_point_2d(double x, double y, double tx, double ty, double rotation, double scale_x, double scale_y, double *result_x, double *result_y);
+
+void rotate_around_point_2d(double x, double y, double center_x, double center_y, double angle, double *result_x, double *result_y);
+
+void rotate_vector_2d(double x, double y, double angle, double *result_x, double *result_y);
+
 #endif
