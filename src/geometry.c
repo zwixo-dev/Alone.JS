2ec636e5a2f8958d92b3cf43e7ec4c80599a7755
SaadDev <zwixozouine@gmail.com>
2026-07-31 23:46:25 +0100

    add geometry_midpoint_x func

--- src/geometry.c
+++ src/geometry.c
@@ -17,3 +17,8 @@ double geometry_distance_3d(double x1, double y1, double z1, double x2, double y
     
     return sqrt(dx * dx + dy * dy + dz * dz);
 }
+
+// geometry_midpoint_x
+double geometry_midpoint_x(double x1, double x2){
+    return (x1 + x2) / 2.0; 
+}
