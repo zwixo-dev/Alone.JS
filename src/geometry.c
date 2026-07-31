eb81655f2e3a6b4d6fa3b4ef5aa4f851e268f38a
SaadDev <zwixozouine@gmail.com>
2026-07-31 23:45:31 +0100

    add geometry_distance_3d  func (i will update it)

--- src/geometry.c
+++ src/geometry.c
@@ -9,4 +9,11 @@ double geometry_distance_2d(double x1, double y1, double x2, double y2){
     return sqrt(dx * dx + dy * dy);
 }
 
-
+// geometry_distance_3d
+double geometry_distance_3d(double x1, double y1, double z1, double x2, double y2, double z2) {
+    double dx = x2 - x1;
+    double dy = y2 - y1;
+    double dz = z2 - z1;
+    
+    return sqrt(dx * dx + dy * dy + dz * dz);
+}
