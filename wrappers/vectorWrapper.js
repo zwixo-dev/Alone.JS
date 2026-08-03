aab6996ec7404f6efd4d3080b83da0f016fff3ef
SaadDev <zwixozouine@gmail.com>
2026-08-03 12:41:11 +0100

    optimise vector_sort_ascending func

--- wrappers/vectorWrapper.js
+++ wrappers/vectorWrapper.js
@@ -74,7 +74,7 @@ Module.onRuntimeInitialized = () => {
     console.log("vector_standard_deviation sample : ",vector_standard_deviation(vA, 1));
     console.log("vector_standard_deviation popu.. : ",vector_standard_deviation(vA, 0));
     console.log("vector_reverse : ", vector_reverse(v));
-    // console.log("vector_sort_ascending : ", vector_sort_ascending(vC.length, vC));
+    console.log("vector_sort_ascending : ", vector_sort_ascending(vC));
     // console.log("vector_sort_descending : ", vector_sort_descending(vC.length, vC));
     // console.log("vector_hadamard_product : ", vector_hadamard_product(vA.length, vA, vB));    
     // console.log("vector_abs : ", vector_abs(vD.length, vD));
@@ -549,18 +549,18 @@ function vector_reverse(vector){
 }
 
 // vector_sort_ascending
-function vector_sort_ascending(size, vector){
-    if(vector.length !== size) return NaN;
+function vector_sort_ascending(vector){
+    if(!Array.isArray(vector) || vector.length === 0) return NaN;
 
-    const inputPointer = allocateMemory(size, vector);
-    const outputPointer = Module._malloc(size * 8);
+    const inputPointer = allocateMemory(vector.length, vector);
+    const outputPointer = Module._malloc(vector.length * 8);
 
-    vectors.vector_sort_ascending(size, inputPointer, outputPointer);
+    vectors.vector_sort_ascending(vector.length, inputPointer, outputPointer);
 
     const vector_sort_ascending = Array.from(
         Module.HEAPF64.subarray(
             outputPointer / 8,
-            outputPointer / 8 + size
+            outputPointer / 8 + vector.length
         )
     );
 
