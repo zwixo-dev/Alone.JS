0b16a20d8df793624e9a0bbe286cafc85061bada
SaadDev <zwixozouine@gmail.com>
2026-08-14 22:03:22 +0100

    update circle_diameter Module.cwrap

--- wrappers/geometryWrapper.js
+++ wrappers/geometryWrapper.js
@@ -29,7 +29,7 @@ Module.onRuntimeInitialized = () => {
         square_diagonal: Module.cwrap("square_diagonal", "number", ["number"]),
         circle_area: Module.cwrap("circle_area", "number", ["number"]),
         circle_circumference: Module.cwrap("circle_circumference", "number", ["number"]),
-        circle_diameter: Module.cwrap("circle_diameter", "number", ["number", "number"]),
+        circle_diameter: Module.cwrap("circle_diameter", "number", ["number"]),
         circle_arc_length: Module.cwrap("circle_arc_length", "number", ["number", "number"]),
         circle_sector_area: Module.cwrap("circle_sector_area", "number", ["number", "number"]),
         ellipse_area: Module.cwrap("ellipse_area", "number", ["number", "number"]),
