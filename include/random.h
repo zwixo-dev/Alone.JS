92519440afe241be189144b0859e17c712926ff6
SaadDev <zwixozouine@gmail.com>
2026-08-15 21:20:17 +0100

    add Basic random headers

---
+++ include/random.h
@@ -0,0 +1,11 @@
+#ifndef ALONE_GEOMETRY_UTILS_H
+#define ALONE_GEOMETRY_UTILS_H
+
+// Basic
+int random_int(void);
+double random_double(void);
+int random_bool(void);
+int random_sign(void);
+
+
+#endif
