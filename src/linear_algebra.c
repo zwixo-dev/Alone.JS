b8a18a362efcc1ed8cd0ae119fa55a506b8f0ddd
SaadDev <zwixozouine@gmail.com>
2026-08-16 23:18:42 +0100

    implement lerp func

--- src/linear_algebra.c
+++ src/linear_algebra.c
@@ -1,3 +1,11 @@
 #include "../include/linear_algebra.h"
 
 
+double lerp(double a, double b, double t) {
+    if (t < 0.0) t = 0.0;
+    if (t > 1.0) t = 1.0;
+
+    return (1.0 - t) * a + t * b;
+}
+
+
