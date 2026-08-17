42cf018551991b4d9770f88760a8c7d143bae644
SaadDev <zwixozouine@gmail.com>
2026-08-17 21:52:15 +0100

    chnage lerp_points_2d func params names

--- include/linear_algebra.h
+++ include/linear_algebra.h
@@ -16,7 +16,7 @@ double clamp(double value, double min, double max);
 
 void lerp_vector(int size, const double *vectorA, const double *vectorB, double t, double *result);
 
-void lerp_points_2d(const double *a, const double *b, double t, double *result);
+void lerp_points_2d(const double *vectorA, const double *vectorB, double t, double *result);
 
 void lerp_points_3d(const double *a, const double *b, double t, double *result);
 
