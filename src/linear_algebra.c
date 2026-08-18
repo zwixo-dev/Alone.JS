27bd919e05b189e510811da6279e85e1cf5f1f05
SaadDev <zwixozouine@gmail.com>
2026-08-18 23:59:57 +0100

    start on rotate_vector_2d funcd

--- src/linear_algebra.c
+++ src/linear_algebra.c
@@ -165,3 +165,7 @@ void rotate_around_point_2d(double x, double y, double center_x, double center_y
     result[0] = center_x + (x - center_x) * cos(angle) - (y - center_y) * sin(angle);
     result[1] = center_y + (x - center_x) * sin(angle) + (y - center_y) * cos(angle);
 }
+
+void rotate_vector_2d(double x, double y, double angle, double *result){
+    
+}
