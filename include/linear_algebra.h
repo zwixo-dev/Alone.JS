969fa4afb8234874d64971bb73ed5bb8009323f7
SaadDev <zwixozouine@gmail.com>
2026-08-24 22:55:17 +0100

    update spherical_to_cartesian header params

--- include/linear_algebra.h
+++ include/linear_algebra.h
@@ -70,7 +70,8 @@ void polar_to_cartesian(double radius, double angle, double *result);
 // void cartesian_to_spherical(double x, double y, double z, double *radius, double *theta, double *phi);
 void cartesian_to_spherical(double x, double y, double z, double *result);
 
-void spherical_to_cartesian(double radius, double theta, double phi, double *x, double *y, double *z);
+// void spherical_to_cartesian(double radius, double theta, double phi, double *x, double *y, double *z);
+void spherical_to_cartesian(double radius, double theta, double phi, double *x, double *y, double *z, double *result);
 
 void cartesian_to_cylindrical(double x, double y, double z, double *radius, double *angle, double *height);
 
