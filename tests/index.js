11bba924227362f0f039ec219f8bba4c47fbb7bc
SaadDev <zwixozouine@gmail.com>
2026-09-10 23:26:50 +0100

    making a basic ball for now

--- tests/index.js
+++ tests/index.js
@@ -22,25 +22,22 @@ canvas.style.height = height + "px";
 canvas.style.backgroundColor = "yellow";
 
 //  default ball
-
-function ball(){
-
-
-    
+function drawBall(ball_x_pos, ball_y_pos, ball_radius, ball_color) {
+    gl.beginPath();
+    gl.arc(ball_x_pos, ball_y_pos, ball_radius, 0, Math.PI * 2);
+    gl.fillStyle = ball_color;
+    gl.fill();
+    gl.stroke();
+    gl.closePath();
 }
 
+// Corrected function call (Positional arguments only)
+drawBall(200+count, 200+count, 40, "red")
 
+// for (let count = 0; count < 10; i++) {
 
-
-
-
-
-
-
-
-
-
-
-
-
+//     drawBall(200+count, 200+count, 40, "red");
+    
+//     count+=1;
+// }
 
