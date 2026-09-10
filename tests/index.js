49e9008caaa59adb5cc72588f43f42baa63582b1
SaadDev <zwixozouine@gmail.com>
2026-09-10 22:56:44 +0100

    trying to build something with the math func (aloneJS)

---
+++ tests/index.js
@@ -0,0 +1,46 @@
+// reset body paading and margin
+
+document.querySelector("body").style.padding=0;
+document.querySelector("body").style.margin=0;
+document.querySelector("body").style.overflow = "hidden";
+
+
+const canvas = document.querySelector(".canvas");
+// gl api
+const gl = canvas.getContext("2d");
+
+// width and height for canvas
+const height = window.innerHeight;
+const width = window.innerWidth; 
+
+canvas.width = width;
+canvas.height = height;
+
+// Set CSS display size
+canvas.style.width = width + "px"; 
+canvas.style.height = height + "px"; 
+canvas.style.backgroundColor = "yellow";
+
+//  default ball
+
+function ball(){
+
+
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
