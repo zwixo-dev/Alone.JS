e1129824afbf5ae9a50b44a1161b2c12ff8e3e13
SaadDev <zwixozouine@gmail.com>
2026-09-23 23:24:12 +0000

    remove Seed functions

--- include/random.h
+++ include/random.h
@@ -30,9 +30,6 @@ void random_coord_3d(double min_x, double max_x,
                      double min_z, double max_z,
                      double *result);
 
-// Seed
-void random_seed(unsigned int seed);
-void random_seed_time(void);
 
 // Character
 char random_digit(void);
