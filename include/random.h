32b6b8e0cad28fffe3a49fe8095306a46f0ebfe7
SaadDev <zwixozouine@gmail.com>
2026-09-23 23:24:37 +0000

    remove Character functions

--- include/random.h
+++ include/random.h
@@ -30,12 +30,4 @@ void random_coord_3d(double min_x, double max_x,
                      double min_z, double max_z,
                      double *result);
 
-
-// Character
-char random_digit(void);
-char random_letter(void);
-char random_digit_range(char min, char max);
-char random_letter_case(int uppercase);
-int random_sign(void);
-
 #endif
